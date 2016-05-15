d58a16e0f8f9ce94b027f11fd9912bf049d61d12
mattyonweb <mattycaos@gmail.com>
2016-05-15 20:19:49 +0000

    Riorganizzato un po' tutto

--- benfatto.py
+++ benfatto.py
@@ -1,6 +1,6 @@
 #f = open("1-premierleague.csv", "r")
 f_out = open("tables.csv", "w")
-import math, random, os
+import math, random, os, collections
 
 class Stats():
 	''' I parametri globali, cioè le statistiche complessive '''
@@ -113,6 +113,12 @@ class Squadra():
 		
 		new_file.write(table)
 
+accepted = ['Reading', 'Sunderland', 'Tottenham', 'Man City', 'QPR', 'Aston Villa', 'Newcastle', 'Southampton', 'Swansea', 'Wigan', 'Everton', 'Chelsea', 'Fulham', 'Stoke', 'Liverpool', 'Arsenal', 'Man United', 'West Brom', 'Norwich', 'West Ham']
+
+for sq_name in accepted:
+	Squadra(sq_name)
+	
+	
 def parser(f):
 	'''Dato un file csv, estrapola tutte le info necessarie. '''		
 
@@ -124,48 +130,39 @@ def parser(f):
 			home_team_name = tokens[1] #nome squadre
 			outs_team_name = tokens[2]
 
+			goals = tokens[3].split("-")
+			home_goals = int(goals[0])
+			outs_goals = int(goals[1])
+			
 			#se non esiste una squadra con il nome indicato nel csv, allora
 			#creane una, sennò prendi quella che c'è già.
 
-			if home_team_name not in Squadra.all_squadre.keys():
-				home_team = Squadra(home_team_name)
-			else:
+			if home_team_name in Squadra.all_squadre.keys():
 				home_team = Squadra.all_squadre[home_team_name]
-
-			if outs_team_name not in Squadra.all_squadre.keys():
-				outs_team = Squadra(outs_team_name)
+				home_team.home_games += 1
+				home_team.gm_h += home_goals
+				home_team.gs_h += outs_goals
+				Stats.TOT_GOALS += home_goals
 			else:
+				pass
+
+			if outs_team_name in Squadra.all_squadre.keys():
 				outs_team = Squadra.all_squadre[outs_team_name]
+				outs_team.outs_games += 1
+				outs_team.gs_t += home_goals
+				outs_team.gm_t += outs_goals
 				
-			#ogni riga corrisponde ad un match --> Per ogni riga bisogna
-			#aggiornare il conteggio dei match in casa/fuori casa delle squadre
-			#coinvolte.
-			home_team.home_games += 1
-			outs_team.outs_games += 1
-			
-			#crea mini-lista con i gol segnati
-			goals = tokens[3].split("-")
-
-			#aggiorna i gol fatti in casa e quelli subiti fuori casa
-			home_goals = int(goals[0])
-			home_team.gm_h += home_goals 
-			outs_team.gs_t += home_goals 
-
-			#aggiorna i gol fatti fuori casa e quelli subiti in casa
-			outs_goals = int(goals[1])
-			outs_team.gm_t += outs_goals
-			home_team.gs_h += outs_goals
-
-			Stats.TOT_GOALS += home_goals + outs_goals
-			
-
+				Stats.TOT_GOALS += outs_goals
+			else:
+				pass
+				
 			#se la squadra di casa vince, aumenta il conteggio globale delle
 			#partite vinte in casa (WON_H)
 			if home_goals > outs_goals:
 				Stats.WON_H += 1
 				
 		#incrementa il numero complessivo di partite giocate
-			Stats.MATCHES += 1
+			Stats.MATCHES += 1 #TODO
 
 def importer(f):
 	#Stats.clear()
@@ -185,14 +182,12 @@ def exporter(f_out):
 	Squadra.return_csv_table("giorgio")
 
 
-#main_season = "12-13.csv"
+filenames = os.listdir("csvs")
 
-#filenames = os.listdir("csvs")
+for filename in filenames:
+	f = open(os.path.join("csvs/" + filename), "r")
+	importer(f)
 
-#for filename in filenames:
-f = open(os.path.join("csvs/" + "12-13.csv"), "r")
-importer(f)
-print(Squadra.all_squadre.keys())
 f.close()
 
 exporter(f_out)
@@ -220,98 +215,150 @@ def poisson_random_number(gamma):
 		p *= random.random()
 	return k - 1
 
-def partita(squadra1, squadra2):
-	squadra1.home_games += 1
-	squadra2.outs_games += 1
-	
-	atk_1 = squadra1.atk_h
-	atk_2 = squadra2.atk_t
-	def_1 = squadra1.def_h
-	def_2 = squadra2.def_t
-	
+class Campionato:
 
-	#questi calcoli di gamma sono molto più realistici di quelli descritti
-	#nel paper, quindi uso questi
-	gamma_1 = atk_1 * def_2 * Stats.AVG_GM_H# + Stats.HOME_ADVG
-	gamma_2 = atk_2 * def_1 * Stats.AVG_GM_T
+	year = 0
+	classifica = {}
 
-	goal_1 = poisson_random_number(gamma_1)
-	goal_2 = poisson_random_number(gamma_2)
+	home_games = [0,0,0]
 
-	Stats.GM_H += goal_1
-	Stats.GM_T += goal_2
+	def __init__(self, squadre):
+		self.squadre = squadre
+		self.campionato = self.totale()
 
-	squadra1.gm_h += goal_1
-	squadra2.gs_h += goal_2
-	squadra2.gm_t += goal_2
-	squadra1.gs_t += goal_1
+		if Campionato.year == 0:
+			for squadra in self.squadre:
+				Campionato.classifica[squadra] = list()
+		else:
+			for squadra in self.squadre:
+				self.squadre[squadra].points = 0
+		Campionato.year += 1
 
-	print(goal_1, goal_2)
+	def andata(self, lista):
+		''' Crea un campionato di andata '''
+		matches = []
+		random.shuffle(lista)
 
-	if goal_1 > goal_2:
-		squadra1.points += 3
-	elif goal_1 < goal_2:
-		squadra2.points += 3
-	else:
-		squadra1.points += 1
-		squadra2.points += 1
+		l1 = lista[0 : int(len(lista)/2)]
+		l2 = lista[int(len(lista)/2) : len(lista)+1]
 
-	calculate_parameters(squadra1)
-	calculate_parameters(squadra2)
+		giornata = []
+		for x in range(len(l1)):
+			giornata.append(l1[x])
+			giornata.append(l2[x])
+		matches.append(giornata)
 
-	Stats.MATCHES += 1
+		for n in range(len(lista)-2):
+			giornata = []
+			l8 = list(l1)
+			l9 = list(l2)
+			l8[2:len(l1)] = l1[1:len(l1)-1]
+			l9[:len(l8)-1] = l2[1:len(l2)]
+			l8[1] = l2[0]
+			l9[-1] = l1[-1]
+			for x in range(len(l8)):
+				giornata.append(l8[x])
+				giornata.append(l9[x])
+			l1 = l8[:]
+			l2 = l9[:]
+			matches.append(giornata)
+			
+		return matches
 
-	return goal_1, goal_2
-	
+	def ritorno(self, campionato_):
+		for giornata in campionato_:
+			for i in range(0, 20, 2):
+				sq_a = giornata[i]
+				sq_b = giornata[i+1]
+				giornata[i] = sq_b
+				giornata[i+1] = sq_a
+
+		return campionato_
+
+	def totale(self):
+		nomi_squadre = list(self.squadre.keys())
+		c_andata = self.andata(nomi_squadre)
+		c_ritorno = self.ritorno(c_andata)
+		return c_andata + c_ritorno
+
+		
+	def partita(self, squadra1, squadra2):
+		squadra1.home_games += 1
+		squadra2.outs_games += 1
+
+		atk_1 = squadra1.atk_h
+		atk_2 = squadra2.atk_t
+		def_1 = squadra1.def_h
+		def_2 = squadra2.def_t
+
+
+		#questi calcoli di gamma sono molto più realistici di quelli descritti
+		#nel paper, quindi uso questi
+		gamma_1 = atk_1 * def_2 * Stats.AVG_GM_H# + Stats.HOME_ADVG
+		gamma_2 = atk_2 * def_1 * Stats.AVG_GM_T
+
+		goal_1 = poisson_random_number(gamma_1)
+		goal_2 = poisson_random_number(gamma_2)
+
+		Stats.GM_H += goal_1
+		Stats.GM_T += goal_2
+
+		squadra1.gm_h += goal_1
+		squadra2.gs_h += goal_2
+		squadra2.gm_t += goal_2
+		squadra1.gs_t += goal_1
+
+		#print(goal_1, goal_2)
+
+		if goal_1 > goal_2:
+			squadra1.points += 3
+			Campionato.home_games[0] += 1
+		elif goal_1 < goal_2:
+			squadra2.points += 3
+			Campionato.home_games[2] += 1
+		else:
+			squadra1.points += 1
+			squadra2.points += 1
+			Campionato.home_games[1] += 1
+
+		calculate_parameters(squadra1)
+		calculate_parameters(squadra2)
+
+		Stats.MATCHES += 1
+
+		return goal_1, goal_2
+
+	def gioca(self):
+		for giornata in self.campionato:
+			for i in range(0, 20, 2):
+				#print(giornata[i], "-", giornata[i+1], end="\t")
+				self.partita(self.squadre[giornata[i]], self.squadre[giornata[i+1]])
+
+		classifica = []
+		for squadra in self.squadre:
+			classifica.append( [squadra, self.squadre[squadra].points] )
+
+		classifica = sorted(classifica, key=lambda x: x[1], reverse = True)
+		for el in classifica:
+			print(el)
+			Campionato.classifica[el[0]].append( classifica.index(el) + 1)
+
+	def mostra_storico(self):
+		for sq in Campionato.classifica:
+			print(sq, collections.Counter(Campionato.classifica[sq]).most_common(3))
+
+		print(Campionato.home_games)
+
+
+
 #-----------
 
-def maches(lista):
-	''' Crea un campionato di andata '''
-	matches = []
-	
-	l1 = lista[0 : int(len(lista)/2)]
-	l2 = lista[int(len(lista)/2) : len(lista)+1]
-
-	giornata = []
-	for x in range(len(l1)):
-		giornata.append(l1[x])
-		giornata.append(l2[x])
-	matches.append(giornata)
-
-	for n in range(len(lista)-2):
-		giornata = []
-		l8 = list(l1)
-		l9 = list(l2)
-		l8[2:len(l1)] = l1[1:len(l1)-1]
-		l9[:len(l8)-1] = l2[1:len(l2)]
-		l8[1] = l2[0]
-		l9[-1] = l1[-1]
-		for x in range(len(l8)):
-			giornata.append(l8[x])
-			giornata.append(l9[x])
-		l1 = l8[:]
-		l2 = l9[:]
-		matches.append(giornata)
-		
-	return matches
-
-def campionato_di_ritorno(campionato_):
-	for giornata in campionato_:
-		for i in range(0, 20, 2):
-			sq_a = giornata[i]
-			sq_b = giornata[i+1]
-			giornata[i] = sq_b
-			giornata[i+1] = sq_a
-
-	return campionato_
-
-
-campionato_andata = maches(list(Squadra.all_squadre.keys()))
+#campionato_andata = maches(list(Squadra.all_squadre.keys()))
 #for giornata in campionato_andata:
 #	print(giornata)
 
-campionato_ritorno = campionato_di_ritorno(campionato_andata)
-campionato = campionato_andata + campionato_ritorno
+#campionato_ritorno = campionato_di_ritorno(campionato_andata)
+#campionato = campionato_andata + campionato_ritorno
 
 print("Stats.MATCHES   = ", str(Stats.MATCHES))
 print("Stats.HOME_ADVG = ", str(Stats.HOME_ADVG))
@@ -321,18 +368,9 @@ print("Stats.AVG_GM_T  = ", str(Stats.AVG_GM_T))
 
 print("-----------------")
 
-for giornata in campionato:
-	for i in range(0, 20, 2):
-		print(giornata[i], "-", giornata[i+1], end="\t")
-		partita(Squadra.all_squadre[giornata[i]], Squadra.all_squadre[giornata[i+1]])
-
-classifica = []
-for squadra in Squadra.all_squadre:
-	classifica.append( [squadra, Squadra.all_squadre[squadra].points] )
-
-classifica = sorted(classifica, key=lambda x: x[1], reverse = True)
-for el in classifica:
-	print(el)
-
-for el in classifica:
-	Squadra.all_squadre[el[0]].posizioni.append(classifica.index(el))
+campionato = Campionato(Squadra.all_squadre)
+for _ in range(40):
+	campionato = Campionato(Squadra.all_squadre)
+	campionato.gioca()
+	print("_____________")
+campionato.mostra_storico()
